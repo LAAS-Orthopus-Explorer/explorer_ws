d63c5ffeadf14cd5365bab08e5dac120a457f857
Thomas Solatges <thomas.solatges@orthopus.com>
2024-07-24 13:57:58 +0200

    (refactor): remove unused tools

--- tools/build-iron-docker_log.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker build --no-cache --progress=plain . 2>&1 | tee build.log
--- tools/run-iron-docker_nosource.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-xhost +
-docker run --rm -it --name ros-pyvesc -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v "/dev/:/dev" -v `pwd`:/src -ti ros-pyvesc-img bash
